77457dceac174c8181b2901a44d6f756b7dac4f5
x-7r3em <vasilegov@gmail.com>
2020-05-10 16:30:02 +0300

    Moved HTTP Server to Code

--- 01. Java Web Basics/02. HTTP Protocol/Projects/JavaWeb_HttpServer/src/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Constants {
-    public static final int PORT = 8080;
-    public static final String HOST = "127.0.0.1";
-    public static final String NEW_LINE = "\r\n";
-}
